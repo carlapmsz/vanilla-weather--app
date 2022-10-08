a482eab44c0d8065b90a04241fe6e09d87cea5ec
carlapmsz <113857100+carlapmsz@users.noreply.github.com>
2022-10-08 22:58:51 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,9 +23,7 @@ function formatDate(timestamp) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=197ef3a642b76eef90e131866f74a0a0&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -69,7 +67,6 @@ function displayForecast(response) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
